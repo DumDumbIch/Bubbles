4971fe729b32397943f9665431fd155d001dda71
DumDumbIch <dumdumbich@gmail.com>
2021-07-21 11:29:52 +0300

    Play on full screen game board

--- app/src/main/java/com/dumdumbich/train/bubbles/ui/MainActivity.kt
+++ app/src/main/java/com/dumdumbich/train/bubbles/ui/MainActivity.kt
@@ -2,11 +2,18 @@ package com.dumdumbich.train.bubbles.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.dumdumbich.train.bubbles.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_main)
     }
 }
